a943024cdb6486bd222e80d7bdfb558415c31eb5
Joseph Perez <joperez@hotmail.fr>
2022-03-24 03:22:52 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # umpmc
+
 Unbounded Multiple Producer Multiple Consumer queue (lock free, obviously)
+
+This algorithm is a POC. It's implemented as a doubly linked list, with a node cache. I've spent my time into writing
+this README instead of code comments, but will maybe find the time to add them later.
+
+## Brief explanation
+
+Nodes are inserted atomically in a LIFO way on the queue *head*, with their *prev* field pointing to the previous *head*
+; *next* pointer of the previous head (or queue's *tail* pointer when no previous head) is set atomically in a third
+step. The second step is to set a unique *index* to the node, which is simply the index of the previous node plus one;
+if the previous node index is not set, *prev* chain is scanned until a set index is find. The tail of the node is
+guaranteed to have its index set: it's set before the node insertion in case of the first node, and it's not possible to
+dequeue a non-tail node while its *next* field is not set (what is done after setting the index of the *next*).
+
+To ensure dequeue uniqueness, queue has an *index* which is incremented atomically when a node is about to be dequeued;
+it must match node's *index*. Because of concurrent assignments, queue's *tail* is not guaranteed to be the exact tail
+of the queue. That's why when the *tail* node index doesn't match, queue is scanned using *next* until a node with
+matching index is found, or the dequeue is rejected. There are two cases:
+
+- The *tail* is the *head*, i.e. there is only one node, then the node is taken out of the queue atomically by setting
+  the *head* ptr. When it fails, i.e. a second node has been inserted concurrently, it goes to the second case.
+- There is more than one node (*tail* is different from *head*), in that case, *tail* node's *next* pointer **MUST** be
+  set, or the dequeue is rejected; it guarantees that *tail*'s *index* is always set (see above) and prevent concurrent
+  set of *next*. If the dequeue is rejected, but queue's *index* has already been incremented (coming from the first
+  case), then the *index* is decremented; decrement failure means that another node is being dequeued, which means that
+  node *next* has concurrently been set (because the other node has been found by scanning from *next*), which means
+  that the dequeue can continue. Then the queue's *tail* is reassigned with compare-and-swap, but it can fail; the
+  operation is retried when the tail concurrently set is not a guaranteed tail (node's *index* equal to the queue *
+  index* minus one, i.e. last dequeue, or new tail with *index* equal to queue index, e.g. enqueue after dequeue of the
+  last node). Finally, the node is invalidated, i.e. *index* is unset and *next* set to null.
+
+Allocated nodes are cached using an atomic LIFO stack. If it prevents unnecessary allocations, it mainly prevents nodes
+to be freed and having dangling *tail* and *next*.
+
+## Analysis
+
+The algorithm seems to work :) Contrary to the
+famous [Dmitry Vyukov's algorithm](https://www.1024cores.net/home/lock-free-algorithms/queues/bounded-mpmc-queue) (which
+was previously part of Rust standard library), it's unbounded. Actually, it could be easy to add a bound, by
+preallocating the cache nodes and preventing further allocations, but this algorithm would surely perform worse than the
+other bounded one.
+
+One of the edge cases is the constraint of having node's *next* set to dequeue it. It can lead to situation where there
+is for example one node fully inserted in the queue, and another one currently being added. In this case, dequeueing the
+first node will fail as long as the second insertion is not finished. However, dequeue has a special
+return `Dequeue::Spin` in this case, to indicate that a spin loop can be used to get a successful dequeue.
+
+Because the cache ensure that there is no dangling pointers, it cannot be shrunk.
+
+## Why?
+
+Unbounded queue is not always a good idea, and there is a bounded algorithm more optimized. Here is the reason why I've
+designed this algorithm.
+
+I wanted to go back to Rust, and I was looking for a project idea. I was thinking about Go channels, wondering why there
+was no (more) standard MPMC in Rust, and had the idea to design one using two atomic indexes (yes, like the bounded
+algorithm, but I had the idea before knowing it, I swear). I did some non-convincing tries — it had been the first time
+for a while that I went deep into atomic, memory ordering, etc. — and when I was finally a little bit satisfied of my
+algorithm.
+
+Then, I looked about the state of the art, and I discovered Dmitry Vyukov's algorithm. I took my time to understand it,
+but I also understood that my tries were completely wrong, and that MPMC was a harder thing than I thought. (I'd also
+discovered that Go channel were using locks, but they were designed by Dmitry Vyukov too, so he knew what he was doing).
+So I tried to adapt my algorithm with what I'd learnt/understood, but I did not manage to find something else than the
+existing bounded MPMC ...
+
+It's frustrating to think hours about something and to realize some people smarter than you have already solved your
+problem in a cleverer way, even if studying the solution is pleasant, of course. I was frustrated and really wanted to
+find something myself, even if it would surely be less optimized, at least it would be original. So I've thought more
+hours, about bounded, but also unbounded algorithm, using array buffer, circular lists, linked lists, etc. And I finally
+found this algorithm. I did not find it on the internet (but I did not crawl all the web), so I think it's original.
+Anyway, it cames from my brain, and that's cool enough for me.
+
---
+++ src/lib.rs
@@ -0,0 +1,362 @@
+use std::mem::MaybeUninit;
+use std::ptr::NonNull;
+use std::sync::atomic::{AtomicBool, AtomicPtr, AtomicUsize, Ordering};
+
+struct NodeIndex {
+    value: MaybeUninit<usize>,
+    is_set: AtomicBool,
+}
+
+impl NodeIndex {
+    fn new() -> Self {
+        NodeIndex {
+            value: MaybeUninit::uninit(),
+            is_set: AtomicBool::new(false),
+        }
+    }
+    fn get(&self) -> Option<usize> {
+        if self.is_set.load(Ordering::Acquire) {
+            Some(unsafe { self.value.assume_init() })
+        } else {
+            None
+        }
+    }
+    fn set(&mut self, value: usize) {
+        debug_assert!(!self.is_set.load(Ordering::Acquire));
+        self.value.write(value);
+        self.is_set.store(true, Ordering::Release);
+    }
+    fn unset(&self) {
+        debug_assert!(self.is_set.load(Ordering::Acquire));
+        self.is_set.store(false, Ordering::Release)
+    }
+}
+
+struct Node<T> {
+    value: MaybeUninit<T>,
+    index: NodeIndex,
+    prev: *mut Node<T>,
+    next: AtomicPtr<Node<T>>,
+}
+
+impl<T> Node<T> {
+    fn new() -> Self {
+        Node {
+            value: MaybeUninit::uninit(),
+            index: NodeIndex::new(),
+            prev: std::ptr::null_mut(),
+            next: AtomicPtr::new(std::ptr::null_mut()),
+        }
+    }
+}
+
+struct Cache<T> {
+    head: AtomicPtr<Node<T>>,
+}
+
+impl<T> Cache<T> {
+    fn new() -> Self {
+        Cache {
+            head: AtomicPtr::new(std::ptr::null_mut()),
+        }
+    }
+    fn pop(&self) -> *mut Node<T> {
+        let mut head = self.head.load(Ordering::Relaxed);
+        while !head.is_null() {
+            match self.head.compare_exchange_weak(
+                head,
+                unsafe { &*head }.prev,
+                Ordering::SeqCst,
+                Ordering::Relaxed,
+            ) {
+                Ok(_) => return head,
+                Err(n) => head = n,
+            }
+        }
+        std::ptr::null_mut()
+    }
+    fn get(&self) -> NonNull<Node<T>> {
+        match NonNull::new(self.pop()) {
+            Some(node) => node,
+            None => unsafe { NonNull::new_unchecked(Box::into_raw(Box::new(Node::new()))) },
+        }
+    }
+    fn put(&self, node: NonNull<Node<T>>) {
+        let mut head = self.head.load(Ordering::Relaxed);
+        loop {
+            unsafe { &mut *node.as_ptr() }.prev = head;
+            match self.head.compare_exchange(
+                head,
+                node.as_ptr(),
+                Ordering::SeqCst,
+                Ordering::Relaxed,
+            ) {
+                Ok(_) => break,
+                Err(h) => head = h,
+            }
+        }
+    }
+    fn clear(&self) {
+        while let Some(node) = NonNull::new(self.pop()) {
+            unsafe { Box::from_raw(node.as_ptr()) };
+        }
+    }
+}
+
+impl<T> Drop for Cache<T> {
+    fn drop(&mut self) {
+        self.clear()
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
+pub enum Dequeue<T> {
+    Empty,
+    Spin,
+    Data(T),
+}
+
+impl<T> Dequeue<T> {
+    fn data(self) -> Option<T> {
+        match self {
+            Dequeue::Data(v) => Some(v),
+            _ => None,
+        }
+    }
+}
+
+impl<T> Into<Option<T>> for Dequeue<T> {
+    fn into(self) -> Option<T> {
+        self.data()
+    }
+}
+
+pub struct Queue<T> {
+    head: AtomicPtr<Node<T>>,
+    tail: AtomicPtr<Node<T>>,
+    index: AtomicUsize,
+    cache: Cache<T>,
+}
+
+impl<T> Queue<T> {
+    pub fn new() -> Self {
+        Queue {
+            head: AtomicPtr::new(std::ptr::null_mut()),
+            tail: AtomicPtr::new(std::ptr::null_mut()),
+            index: AtomicUsize::new(0),
+            cache: Cache::new(),
+        }
+    }
+
+    pub fn enqueue(&self, value: T) {
+        let node = unsafe { self.cache.get().as_mut() };
+        node.value.write(value);
+        let mut head = self.head.load(Ordering::Relaxed);
+        let mut index_has_been_set = false;
+        loop {
+            node.prev = head;
+            if head.is_null() {
+                node.index.set(self.index.load(Ordering::Relaxed));
+                index_has_been_set = true;
+            } else if index_has_been_set {
+                node.index.unset();
+                index_has_been_set = false;
+            }
+            match self
+                .head
+                .compare_exchange(head, node, Ordering::SeqCst, Ordering::Relaxed)
+            {
+                Ok(_) => break,
+                Err(h) => head = h,
+            }
+        }
+        if !head.is_null() {
+            let mut prev = head;
+            let mut offset = 0;
+            loop {
+                debug_assert!(!prev.is_null());
+                match unsafe { &*prev }.index.get() {
+                    Some(i) => {
+                        node.index.set(i + offset + 1);
+                        break;
+                    }
+                    None => {
+                        prev = unsafe { &*prev }.prev;
+                        offset += 1;
+                    }
+                }
+            }
+            unsafe { &*head }.next.store(node, Ordering::Release);
+        } else {
+            self.tail.store(node, Ordering::SeqCst);
+        }
+    }
+
+    fn set_tail(&self, node: &mut Node<T>, mut tail: *mut Node<T>, next: *mut Node<T>) -> T {
+        debug_assert!(tail.is_null() || unsafe { &*tail }.index.get().is_some());
+        while let Err(t) =
+        self.tail
+            .compare_exchange(tail, next, Ordering::SeqCst, Ordering::Relaxed)
+        {
+            let index = self.index.load(Ordering::Relaxed);
+            if node.index.get() != Some(index - 1) || (!t.is_null()
+                && unsafe { &*t }.prev.is_null()
+                && unsafe { &*t }.index.get() == Some(index))
+            {
+                break;
+            }
+            tail = t
+        }
+        let value = unsafe { node.value.assume_init_read() };
+        node.index.unset();
+        node.next.store(std::ptr::null_mut(), Ordering::Relaxed);
+        self.cache.put(node.into());
+        value
+    }
+
+    pub fn dequeue(&self) -> Dequeue<T> {
+        let mut index = self.index.load(Ordering::Relaxed);
+        let mut tail = self.tail.load(Ordering::Relaxed);
+        while !tail.is_null() {
+            let node = unsafe { &mut *tail };
+            let tail_index = match node.index.get() {
+                Some(i) => i,
+                None => return Dequeue::Empty,
+            };
+            let head = self.head.load(Ordering::Relaxed);
+            let mut next = node.next.load(Ordering::Relaxed);
+            if next.is_null() && tail != head {
+                return Dequeue::Spin;
+            }
+            if index == tail_index
+                && match self.index.compare_exchange(
+                index,
+                index + 1,
+                Ordering::SeqCst,
+                Ordering::Relaxed,
+            ) {
+                Ok(_) => true,
+                Err(i) => {
+                    index = i;
+                    false
+                }
+            }
+            {
+                if tail == head {
+                    if self
+                        .head
+                        .compare_exchange(
+                            head,
+                            std::ptr::null_mut(),
+                            Ordering::SeqCst,
+                            Ordering::Relaxed,
+                        )
+                        .is_ok()
+                    {
+                        return Dequeue::Data(self.set_tail(node, tail, next));
+                    } else {
+                        next = node.next.load(Ordering::Acquire);
+                        if next.is_null() && self
+                            .index
+                            .compare_exchange(index + 1, index, Ordering::SeqCst, Ordering::Relaxed)
+                            .is_ok()
+                        {
+                            return Dequeue::Spin;
+                        } else {
+                            next = node.next.load(Ordering::Acquire);
+                        }
+                    }
+                }
+                debug_assert!(!next.is_null());
+                return Dequeue::Data(self.set_tail(node, tail, next));
+            } else {
+                tail = next;
+            }
+        }
+        Dequeue::Empty
+    }
+}
+
+impl<T> Drop for Queue<T> {
+    fn drop(&mut self) {
+        while let Dequeue::Data(_) = self.dequeue() {}
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+    use std::sync::{Arc, Mutex};
+
+    use crate::{Dequeue, Queue};
+
+    #[test]
+    fn synchronous() {
+        let queue = Queue::new();
+        assert_eq!(queue.dequeue(), Dequeue::Empty);
+        queue.enqueue(0);
+        assert_eq!(queue.dequeue(), Dequeue::Data(0));
+        queue.enqueue(1);
+        assert_eq!(queue.dequeue(), Dequeue::Data(1));
+        queue.enqueue(2);
+        queue.enqueue(3);
+        assert_eq!(queue.dequeue(), Dequeue::Data(2));
+        queue.enqueue(4);
+        queue.enqueue(5);
+        assert_eq!(queue.dequeue(), Dequeue::Data(3));
+        assert_eq!(queue.dequeue(), Dequeue::Data(4));
+        assert_eq!(queue.dequeue(), Dequeue::Data(5));
+        assert_eq!(queue.dequeue(), Dequeue::Empty);
+    }
+
+    fn test_asynchronous(nb_values: usize) {
+        let start = std::time::Instant::now();
+        let queue = Arc::new(Queue::new());
+        let vec = Arc::new(Mutex::new(Vec::new()));
+        let mut threads = vec![];
+        for _ in 0..nb_values {
+            let queue = queue.clone();
+            let vec = vec.clone();
+            threads.push(std::thread::spawn(move || loop {
+                if let Dequeue::Data(n) = queue.dequeue() {
+                    vec.lock().unwrap().push(n);
+                    break;
+                } else if std::time::Instant::now().duration_since(start).as_secs() > 30 {
+                    break;
+                }
+            }));
+        }
+        std::thread::sleep(std::time::Duration::from_micros(10));
+        for i in 0..nb_values {
+            let queue = queue.clone();
+            threads.push(std::thread::spawn(move || {
+                queue.enqueue(i);
+            }));
+        }
+        for t in threads {
+            t.join().unwrap();
+        }
+        assert_eq!(
+            Arc::try_unwrap(vec)
+                .unwrap()
+                .into_inner()
+                .unwrap()
+                .into_iter()
+                .collect::<HashSet<_>>()
+                .len(),
+            nb_values
+        );
+    }
+
+    #[test]
+    fn asynchronous() {
+        let range = 0..100;
+        let tests = [2, 8, 32];
+        for nb_values in tests {
+            for _ in range.clone() {
+                test_asynchronous(nb_values);
+            }
+        }
+    }
+}
